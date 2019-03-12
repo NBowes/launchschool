e6993ab658d72dee7a7fb2af7461aa3091ce4944
NBowes <nathen.bowes@shopify.com>
2019-03-11 17:32:36 -0700

    finish conditionals

---
+++ ruby_basics/conditionals/cool_numbers.rb
@@ -0,0 +1,8 @@
+number = rand(10)
+
+if number == 5 # one '=' would make this true every time
+  puts '5 is a cool number!'
+else
+  puts 'Other numbers are cool too!'
+end
+puts number = 5
---
+++ ruby_basics/conditionals/number.rb
@@ -0,0 +1,7 @@
+# Is a number truthy?
+
+if number
+  puts "I am truthy"
+else
+  puts "No"
+end
