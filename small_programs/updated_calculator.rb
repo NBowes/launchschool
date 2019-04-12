8b174e66b584e081e0319ee26ef23967912b7f42
NBowes <nathen.bowes@shopify.com>
2019-04-11 20:11:50 -0700

    update calculator with rubocop

--- small_programs/updated_calculator.rb
+++ small_programs/updated_calculator.rb
@@ -3,98 +3,89 @@
 # print result with 'adding..' message
 # ask for user's name
 # ask if you want to do another calculation
-# goodbye message 
+# goodbye message
 
 def valid_number?(num)
   num.to_i != 0
 end
 
-def operator_message(op)
-  case op
-  when "1"
-    puts "Adding numbers..."
-  when "2"
-    puts "Subtracting numbers..."
-  when "3"
-    puts "Multiplying numbers..."
-  when "4"
-    puts "Dividing numbers..."
+def operator_message(operator)
+  case operator
+  when '1'
+    puts 'Adding numbers...'
+  when '2'
+    puts 'Subtracting numbers...'
+  when '3'
+    puts 'Multiplying numbers...'
+  when '4'
+    puts 'Dividing numbers...'
   end
 end
 
-puts "What is your name?"
+puts 'What is your name?'
 name = nil
 
 loop do
   name = gets.chomp
 
-  if name.empty?
-    puts "Plese make sure to enther your name"
-  else
-    break
-  end
+  puts 'Plese make sure to enther your name' if name.empty?
+  break if name
 end
 
 puts "Hey #{name}!"
 loop do
   number1 = nil
   loop do
-    puts "What is your first number?"
+    puts 'What is your first number?'
     number1 = gets.chomp
 
-    if valid_number?(number1)
-      break
-    else
-      puts "Hmm.. that number doesn't look right."
-    end
+    break if valid_number?(number1)
+
+    puts "Hmm.. that number doesn't look right."
   end
 
   number2 = nil
   loop do
-    puts "What is your second number?"
+    puts 'What is your second number?'
     number2 = gets.chomp
 
-    if valid_number?(number2)
-      break
-    else
-      puts "Hmm.. that number doesn't look right."
-    end
+    break if valid_number?(number2)
+
+    puts "Hmm.. that number doesn't look right."
   end
 
-  puts "What operation should we do? 1.add, 2.subtract, 3.multiply, 4.divide?"
+  puts 'What operation should we do? 1.add, 2.subtract, 3.multiply, 4.divide?'
 
   operator = nil
   loop do
     operator = gets.chomp
 
-    if %(1,2,3,4).include?(operator)
-      break
-    else
-      puts "You must choose 1,2,3, or 4"
-    end
+    break if %(1,2,3,4).include?(operator)
+
+    puts 'You must choose 1,2,3, or 4'
   end
 
   operator_message(operator)
 
   result = case operator
-  when "1"
-    number1.to_i + number2.to_i
-  when "2"
-    number1.to_i - number2.to_i
-  when "3"
-    number1.to_i * number2.to_i
-  when "4"
-    number1.to_f / number2.to_f
-  end
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
 
   puts "The result is #{result}"
-  puts "Do you want to do another calculation? (N to quit)"
-    answer = gets.chomp
+  puts 'Do you want to do another calculation? (N to quit)'
+  answer = gets.chomp
 
-    break if answer.downcase == 'n'
+  break if answer.casecmp('n').zero?
 end
 
 puts <<-MSG
-Thanks for using the calculator #{name}.
-To start it again type 'ruby updated_calculator.rb' and press enter.
+  Thanks for using the calculator #{name}.
+  To start it again type 'ruby updated_calculator.rb' and press enter.
 MSG
