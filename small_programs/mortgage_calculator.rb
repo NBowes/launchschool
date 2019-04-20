2e005bd70727205790f6bb2e9bf21f8de6a445a8
NBowes <nathen.bowes@shopify.com>
2019-04-19 22:42:52 -0700

    added mortgage calculator validation

--- small_programs/mortgage_calculator.rb
+++ small_programs/mortgage_calculator.rb
@@ -16,22 +16,46 @@ def prompt(message)
   puts message
 end
 
+def monthly_pay(loan_amount, monthly_rate, montly_loan_duration)
+  denominator = 1 - (1 + monthly_rate)**-montly_loan_duration
+  loan_amount.to_f * monthly_rate / denominator
+end
+
 prompt('Welcome to the Mortgage Calculator')
 
-prompt('What is the loan total?')
-loan_amount = gets.chomp.to_i
+loan_amount = nil
+loop do
+  prompt('What is the loan total?')
+  loan_amount = gets.chomp.to_i
 
-prompt('What is the annual interest rate')
-prompt('(Example 5 for 5% or 2.5 for 2.5%)')
-apr = gets.chomp.to_f
+  break if loan_amount > 0
 
-prompt('How long is the loan for in years?')
-years = gets.chomp
+  prompt('Please input a valid loan amount.')
+end
 
-annual_interest = apr.to_f / 100
-monthly_interest_rate = annual_interest / 12
+apr = nil
+loop do
+  prompt('What is the annual interest rate')
+  prompt('(Example 5 for 5% or 2.5 for 2.5%)')
+  apr = gets.chomp.to_f
+  break if apr.to_i > 0
+
+  prompt('Please input a valid interest rate.')
+end
+
+years = nil
+loop do
+  prompt('How long is the loan for in years?')
+  years = gets.chomp
+  break unless years.empty? || years.to_i < 0
+
+  prompt('Please enter a valid number of years.')
+end
+
+annual_rate = apr.to_f / 100
+monthly_rate = annual_rate / 12
 montly_loan_duration = years.to_i * 12
 
-month_pay = loan_amount.to_f * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-montly_loan_duration)))
+month_pay = monthly_pay(loan_amount, monthly_rate, montly_loan_duration)
 
-prompt(month_pay.round(2))
+prompt("Your monthly payment is $#{month_pay.round(2)}")
