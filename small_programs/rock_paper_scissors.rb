b3170436f7892af0e11609b9488b1821e6e9ecc2
NBowes <nathen.bowes@shopify.com>
2019-04-26 09:50:06 -0700

    updated rps to have a score - added lizard and spock

--- small_programs/rock_paper_scissors.rb
+++ small_programs/rock_paper_scissors.rb
@@ -1,5 +1,19 @@
-VALID_CHOICES = %w[rock paper scissors].freeze
+VALID_CHOICES = %w[rock paper scissors lizard spock].freeze
 VALID_ANSWER = %w[y n].freeze
+RULES = %w[
+  Scissors cuts
+  Paper covers
+  Rock crushes
+  Lizard poisons
+  Spock smashes
+  Scissors decapitates
+  Lizard eats
+  Paper disproves
+  Spock vaporizes
+  Rock crushes Scissors
+].freeze
+
+score = { player: 0, computer: 0 }
 
 def prompt(message)
   puts message
@@ -8,29 +22,49 @@ end
 def you_win(choice, computer)
   choice == 'rock' && computer == 'scissors' ||
     choice == 'scissors' && computer == 'paper' ||
-    choice == 'paper' && computer == 'rock'
+    choice == 'paper' && computer == 'rock' ||
+    choice == 'rock' && computer == 'lizard' ||
+    choice == 'lizard' && computer == 'spock' ||
+    choice == 'scissors' && computer == 'lizard' ||
+    choice == 'spock' && computer == 'scissors' ||
+    choice == 'lizard' && computer == 'paper' ||
+    choice == 'paper' && computer == 'spock' ||
+    choice == 'spock' && computer == 'rock'
 end
 
 def computer_win(choice, computer)
   choice == 'rock' && computer == 'paper' ||
     choice == 'paper' && computer == 'scissors' ||
-    choice == 'scissors' && computer == 'rock'
+    choice == 'scissors' && computer == 'rock' ||
+    choice == 'lizard' && computer == 'rock' ||
+    choice == 'spock' && computer == 'lizard' ||
+    choice == 'lizard' && computer == 'scissors' ||
+    choice == 'scissors' && computer == 'spock' ||
+    choice == 'paper' && computer == 'lizard' ||
+    choice == 'spock' && computer == 'paper' ||
+    choice == 'rock' && computer == 'spock'
 end
 
-def winner(choice, computer)
+def winner(choice, computer, score)
   if you_win(choice, computer)
     prompt('You win!')
+    score[:player] += 1
   elsif computer_win(choice, computer)
     prompt('Computer wins :(')
+    score[:computer] += 1
   else
     prompt("It's a tie")
   end
 end
 
-def display_results(choice, computer)
+def display_round_results(choice, computer, score)
   prompt("You chose: #{choice}")
   prompt("Computer chose: #{computer}")
-  winner(choice, computer)
+  winner(choice, computer, score)
+end
+
+def display_score(score)
+  prompt("Player: #{score[:player]}, Computer: #{score[:computer]}")
 end
 
 loop do
@@ -46,18 +80,10 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-  display_results(choice, computer_choice)
-  prompt('Do you want to play again? (y/n)')
-  answer = nil
-  loop do
-    answer = gets.chomp
+  display_round_results(choice, computer_choice, score)
+  display_score(score)
 
-    break if VALID_ANSWER.include?(answer)
-
-    prompt("Please choice #{VALID_ANSWER.join(', ')} (n to quit)")
-  end
-
-  break if answer.casecmp('n').zero?
+  break if score[:player] == 5 || score[:computer] == 5
 end
 
 prompt('Thanks for playing!')
