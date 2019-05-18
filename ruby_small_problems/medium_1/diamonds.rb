2db85eb6da252b7450c987f0d088059261c598f7
NBowes <nathen.bowes@shopify.com>
2019-05-17 18:32:51 -0700

    rubocop diamonds

--- ruby_small_problems/medium_1/diamonds.rb
+++ ruby_small_problems/medium_1/diamonds.rb
@@ -4,14 +4,14 @@
 def diamond(num)
   stars = []
   1.upto(num) do |index|
-    star_row = "*" * index
+    star_row = '*' * index
     stars << star_row.center(num)
   end
   num.downto(1) do |index|
-    star_row = "*" * (index)
+    star_row = '*' * index
     stars << star_row.center(num)
   end
-  stars.each {|star| puts star}
+  stars.each { |star| puts star }
 end
 
 diamond(5)
