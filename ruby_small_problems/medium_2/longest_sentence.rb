3c43495cf303693c30b1db654bb9105f13bda65d
NBowes <nathen.bowes@shopify.com>
2019-06-13 23:36:26 -0700

    rubocop medium 2

--- ruby_small_problems/medium_2/longest_sentence.rb
+++ ruby_small_problems/medium_2/longest_sentence.rb
@@ -3,7 +3,7 @@
 
 f = File.read('longest_sentence.txt')
 sentences = f.split(/\.|!|\?/)
-longest_sentence = sentences.max_by { |sentence| sentence.split.size}
+longest_sentence = sentences.max_by { |sentence| sentence.split.size }
 longest_sentence = longest_sentence.strip
 number_of_words = longest_sentence.split.size
 
@@ -13,9 +13,7 @@ puts "The longest sentence has #{number_of_words} words in it"
 def longest(sentences)
   longest = 0
   sentences.each do |sentence|
-    if sentence.strip.split.size > longest
-      longest = sentence.strip.split.size
-    end
+    longest = sentence.strip.split.size if sentence.strip.split.size > longest
   end
   longest
 end
