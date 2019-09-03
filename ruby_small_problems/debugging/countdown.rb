929e6ff8444e3752b140dc4495b5f2f313c3116a
NBowes <nathen.bowes@shopify.com>
2019-09-03 12:06:20 -0700

    started debugging

---
+++ ruby_small_problems/debugging/countdown.rb
@@ -0,0 +1,27 @@
+def decrease(counter)
+  counter -= 1
+end
+
+counter = 10
+
+10.times do
+  puts counter
+  decrease(counter)
+end
+
+puts 'LAUNCH!'
+
+## fixed
+
+def decrease(counter)
+  counter -  1
+end
+
+counter = 10
+
+10.times do
+  puts counter
+  counter = decrease(counter)
+end
+
+puts 'LAUNCH!'
