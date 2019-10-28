8a3b3518cdfb407669b3225d86edb31013ef5a6f
Nathen Bowes <nathen.bowes@shopify.com>
2019-10-28 13:28:05 -0700

    add MyCar class

---
+++ oop/launch_school_oop_book/classes_and_objects_1/mycar.rb
@@ -0,0 +1,37 @@
+class MyCar
+
+    def initialize(year, color, model)
+        @year = year
+        @color = color
+        @model = model
+        @current_speed = 0
+    end
+
+    def speed_up(speed)
+        @current_speed += speed
+        puts "The car speeds up to #{@current_speed}"
+    end
+
+    def brake(speed)
+        @current_speed -= speed
+        puts "The car is slowing down to #{@current_speed}"
+    end
+
+    def current_speed
+        puts "The car is now going #{@current_speed}"
+    end
+
+    def shut_off_car
+        @current_speed = 0
+        puts "Let's park this car"
+    end
+end
+
+westy = MyCar.new('1991', 'White','Volkswagen')
+westy.speed_up(10)
+westy.current_speed
+westy.speed_up(10)
+westy.speed_up(10)
+westy.current_speed
+westy.brake(20)
+westy.current_speed
