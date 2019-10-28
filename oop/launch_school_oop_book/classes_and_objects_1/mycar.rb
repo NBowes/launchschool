ad73fdcbc825fa1170672f1181d9733d168d350b
Nathen Bowes <nathen.bowes@shopify.com>
2019-10-28 13:37:17 -0700

    added getter and setter methods to MyCar

--- oop/launch_school_oop_book/classes_and_objects_1/mycar.rb
+++ oop/launch_school_oop_book/classes_and_objects_1/mycar.rb
@@ -1,4 +1,6 @@
 class MyCar
+    attr_accessor :color
+    attr_reader :year
 
     def initialize(year, color, model)
         @year = year
@@ -25,9 +27,16 @@ class MyCar
         @current_speed = 0
         puts "Let's park this car"
     end
+
+    def spray_paint(color)
+        self.color = color
+        puts "The car got painted the color #{color}"
+    end
 end
 
 westy = MyCar.new('1991', 'White','Volkswagen')
+puts westy.color
+puts westy.year
 westy.speed_up(10)
 westy.current_speed
 westy.speed_up(10)
@@ -35,3 +44,5 @@ westy.speed_up(10)
 westy.current_speed
 westy.brake(20)
 westy.current_speed
+westy.shut_off_car
+westy.spray_paint('Sand')
