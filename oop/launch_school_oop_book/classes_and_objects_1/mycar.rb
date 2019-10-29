e41c9d6d1ea71309ffae88b1beac8d05aa58a122
Nathen Bowes <nathen.bowes@shopify.com>
2019-10-29 16:53:37 -0700

    update spacing for mycar.rb

--- oop/launch_school_oop_book/classes_and_objects_1/mycar.rb
+++ oop/launch_school_oop_book/classes_and_objects_1/mycar.rb
@@ -1,73 +1,73 @@
 require 'Time'
 module Carryable
-    def can_carry?(pounds)
-        pounds < 2000 ? true : false
-    end
+  def can_carry?(pounds)
+    pounds < 2000 ? true : false
+  end
 end
 
 class Vehicle
-    @@number_of_vehicles = 0
-    attr_accessor :color
-    attr_reader :year, :model
+  @@number_of_vehicles = 0
+  attr_accessor :color
+  attr_reader :year, :model
 
-    def initialize(year, color, model)
-        @@number_of_vehicles += 1
-        @year = year
-        @color = color
-        @model = model
-        @current_speed = 0
-    end
+  def initialize(year, color, model)
+    @@number_of_vehicles += 1
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
 
-    def age
-        puts "The #{self.model} is #{years_old} years old."
-    end
+  def age
+    puts "The #{self.model} is #{years_old} years old."
+  end
 
-    def speed_up(speed)
-        @current_speed += speed
-        puts "The car speeds up to #{@current_speed}"
-    end
+  def speed_up(speed)
+    @current_speed += speed
+    puts "The car speeds up to #{@current_speed}"
+  end
 
-    def brake(speed)
-        @current_speed -= speed
-        puts "The car is slowing down to #{@current_speed}"
-    end
+  def brake(speed)
+    @current_speed -= speed
+    puts "The car is slowing down to #{@current_speed}"
+  end
 
-    def current_speed
-        puts "The car is now going #{@current_speed}"
-    end
+  def current_speed
+    puts "The car is now going #{@current_speed}"
+  end
 
-    def shut_off_car
-        @current_speed = 0
-        puts "Let's park this car"
-    end
+  def shut_off_car
+    @current_speed = 0
+    puts "Let's park this car"
+  end
 
-    def spray_paint(color)
-        self.color = color
-        puts "The car got painted the color #{color}"
-    end
+  def spray_paint(color)
+    self.color = color
+    puts "The car got painted the color #{color}"
+  end
 
-    def self.gas_mileage(litres, kilometers)
-        puts "Gas Mileage: #{litres}L per #{kilometers} kilometers"
-    end
+  def self.gas_mileage(litres, kilometers)
+    puts "Gas Mileage: #{litres}L per #{kilometers} kilometers"
+  end
 
-    def self.number_of_vehicles
-        puts "This program created #{@@number_of_vehicles} vehicles"
-    end
+  def self.number_of_vehicles
+    puts "This program created #{@@number_of_vehicles} vehicles"
+  end
 
-    private
+  private
 
-    def years_old
-        Time.now.year - self.year
-    end
+  def years_old
+    Time.now.year - self.year
+  end
 end
 
 class MyCar < Vehicle
-   DOORS = 4
+  DOORS = 4
 end
 
 class MyTruck < Vehicle
-    include Carryable
-    DOORS = 2
+  include Carryable
+  DOORS = 2
 end
 
 westy = MyCar.new(1991, 'White','Volkswagen')
