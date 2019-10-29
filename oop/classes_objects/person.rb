925b4e47c07c48b07b3256bd9fa23e9595f1404f
Nathen Bowes <nathen.bowes@shopify.com>
2019-10-29 10:50:34 -0700

    added name instance method and removed attr_accessor for :name

--- oop/classes_objects/person.rb
+++ oop/classes_objects/person.rb
@@ -1,13 +1,20 @@
 class Person
-  attr_accessor :name
+  attr_accessor :first_name, :last_name
 
-  def initialize(name)
-    @name = name
+  def initialize(full_name)
+    name_parts = full_name.split
+    @first_name = name_parts.first
+    @last_name = name_parts.size > 1 ? name_parts[-1] : ' '
+  end
+
+  def name
+    "#{@first_name} #{@last_name}".strip
   end
 end
 
-
-bob = Person.new('bob')
-bob.name
-bob.name = 'Robert'
-bob.name
+bob = Person.new('Robert')
+p bob.name
+p bob.first_name
+p bob.last_name
+bob.last_name = 'Smith'
+p bob.name
