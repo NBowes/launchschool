c63b039ded10b4b23771010a95369bc19fb69e0f
Nathen Bowes <nathen.bowes@shopify.com>
2019-10-29 09:25:21 -0700

    fix spacing and add Walkable module

--- oop/oop_basics/cat.rb
+++ oop/oop_basics/cat.rb
@@ -1,12 +1,25 @@
-class Cat
-    def initialize(name)
-        @name = name
-    end
+module Walkable
+  def walk
+    puts "Let's go for a walk."
+  end
+end
 
-    def greet
-        puts "Hello, I'm #{@name}."
-    end
+class Cat
+  include Walkable
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello, I'm #{name}."
+  end
 end
 
 kitty = Cat.new('liam')
 kitty.greet
+kitty.name = 'Liam'
+kitty.greet
+kitty.walk
