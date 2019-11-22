478993d1a1fb23318aecd098d003e5140405ce13
Nathen Bowes <nathen.bowes@shopify.com>
2019-11-21 17:03:04 -0800

    add choose method to Player class, initialize with player type

--- oop/rps/oop_rps.rb
+++ oop/rps/oop_rps.rb
@@ -1,38 +1,54 @@
+class Player
+  attr_accessor :move
+
+  def initialize(player_type = :human)
+    @player_type = player_type
+    @move = nil
+  end
+
+  def choose
+    if human?
+      choice = nil
+      loop do
+        puts "Please choose rock, paper, scissors:"
+        choice = gets.chomp
+        break if ['rock', 'paper', 'scissors'].include? choice
+        puts 'Sorry, invalid choice.'
+      end
+      self.move = choice
+    else
+      self.move = ['rock', 'paper', 'scissors'].sample
+    end
+  end
+
+  def human?
+    @player_type == :human
+  end
+end
+
 class RPSGame
   attr_accessor :human, :computer
 
-  def initialiize
+  def initialize
     @human = Player.new
-    @computer = Player.new
+    @computer = Player.new(:comuter)
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors!"
   end
 
   def play
     display_welcome_message
-    human_choose_move
-    computer_choose_move
+    human.choose
+    computer.choose
     display_winner
     display_goodbye_message
   end
 end
 
-class Player
-  def initialiize(name)
-    @name = name
-  end
-
-  def choose
-  end
-end
-
-class Move
-  def initialiize
-  end
-end
-
-class Rule
-  def initialiize
-  end
-end
-
-def compare(move1, move2)
-end
+RPSGame.new.play
