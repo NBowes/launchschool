b67be1cfbc0e72037483bdfc94097071b8a2ce10
Nathen Bowes <nathen.bowes@shopify.com>
2019-11-21 17:17:26 -0800

    add display_winner method

--- oop/rps/oop_rps.rb
+++ oop/rps/oop_rps.rb
@@ -42,6 +42,26 @@ class RPSGame
     puts "Thanks for playing Rock, Paper, Scissors!"
   end
 
+  def display_winner
+    puts "You chose: #{human.move}"
+    puts "The computer chose: #{computer.move}"
+
+    case human.move
+    when 'rock'
+      puts 'Its a tie.' if computer.move == 'rock'
+      puts 'You won!' if computer.move == 'scissors'
+      puts 'You lost :(' if computer.move == 'paper'
+    when 'paper'
+      puts 'Its a tie.' if computer.move == 'paper'
+      puts 'You won!' if computer.move == 'rock'
+      puts 'You lost :(' if computer.move == 'scissors'
+    when 'scissors'
+      puts 'Its a tie.' if computer.move == 'scissors'
+      puts 'You won!' if computer.move == 'paper'
+      puts 'You lost :(' if computer.move == 'rock'
+    end
+  end
+
   def play
     display_welcome_message
     human.choose
