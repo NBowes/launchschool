18829200facc0f26d4e720afae0f42a43641e8ae
Nathen Bowes <nathen.bowes@shopify.com>
2020-07-29 22:55:25 -0700

    start rubocop refactor

--- oop/medium/number_guesser.rb
+++ oop/medium/number_guesser.rb
@@ -1,22 +1,24 @@
-=begin
-Create an object-oriented number guessing class for numbers in the range 1 to 100, with a limit of 7 guesses per game. The game should play like this:
+# frozen_string_literal: true
 
-game = GuessingGame.new
-game.play
+# Create an object-oriented number
+# guessing class for numbers in
+# the range 1 to 100
+# with a limit of 7 guesses per game.
+# The game should play like this:
 
-You have 7 guesses remaining.
-Enter a number between 1 and 100: 104
-Invalid guess. Enter a number between 1 and 100: 50
-Your guess is too low.
+# You have 7 guesses remaining.
+# Enter a number between 1 and 100: 104
+# Invalid guess. Enter a number between 1 and 100: 50
+# Your guess is too low.
 
-You have 6 guesses remaining.
-Enter a number between 1 and 100: 75
-Your guess is too low.
+# You have 6 guesses remaining.
+# Enter a number between 1 and 100: 75
+# Your guess is too low.
 
-You have 5 guesses remaining.
-Enter a number between 1 and 100: 85
-Your guess is too high.
-=end
+# You have 5 guesses remaining.
+# Enter a number between 1 and 100: 85
+# Your guess is too high.
+
 class GuessingGame
   attr_accessor :guesses
 
@@ -25,46 +27,63 @@ class GuessingGame
     @guesses = 7
   end
 
-  def play
-    set_secret
-    guess
-  end
-
   def valid_guess?(guess)
-    guess.class == Integer && (guess > 1 && guess < 100)
+    (guess.is_a? Integer) && (guess > 1 && guess < 100)
   end
 
-  def guess
-    guess = nil
-    loop do
-      loop do
-        puts "You have #{self.guesses} guesses remaining."
-        puts 'Enter a number between 1 and 100: '
-        guess = gets.chomp.to_i
-        break if valid_guess?(guess)
-        puts 'Invalid guess'
-      end
-      winner?(guess) ? display_message('You got it!') : guess_logic(guess)
-      reduce_guesses
-      break if guesses == 0 || winner?(guess)
+  def no_guesses
+    if guesses.zero?
+      display_message('You have no more guesses. You lose.')
+    else
+      false
     end
   end
 
-  def winner?(guess)
-    guess == @secret_number
+  def guess_logic
+    guess = nil
+    loop do
+      loop do
+        user_guess_message
+        guess = gets.chomp.to_i
+        break if valid_guess?(guess)
+
+        display_message('Invalid guess')
+      end
+      break if no_guesses || winner?(guess)
+    end
   end
 
   def display_message(message)
     puts message
   end
 
-  def guess_logic(guess)
+  def guess_message(guess)
     display_message(guess > @secret_number ? 'Too high' : 'Too low')
+    reduce_guesses
+  end
+
+  def play
+    set_secret
+    guess_logic
   end
 
   def reduce_guesses
     self.guesses -= 1
-    return 'You lose.' if guesses == 0
+    return 'You lose.' if guesses.zero?
+  end
+
+  def user_guess_message
+    display_message("You have #{guesses} guesses remaining.")
+    display_message('Enter a number between 1 and 100: ')
+  end
+
+  def winner?(guess)
+    if guess == @secret_number
+      display_message('You got it!')
+      true
+    else
+      guess_message(guess)
+    end
   end
 
   private
