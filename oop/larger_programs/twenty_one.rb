5ed22beda021111707fdef09ce1369bd866f8f10
Nathen Bowes <nathen.bowes@shopify.com>
2020-08-23 18:33:11 -0700

    conitnue building - still more bugs

--- oop/larger_programs/twenty_one.rb
+++ oop/larger_programs/twenty_one.rb
@@ -33,33 +33,56 @@ class Card
     @suit = suit
     @value = value
   end
-end
 
-class Deck
-  attr_reader :cards
-  def initialize
-    @cards = []
-    suits = %w[H D S C]
-    card_values = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
-    suits.each do |suit|
-      card_values.each do |value|
-        cards << Card.new(suit, value)
-      end
+  def value
+    case @value
+    when 'A' then 'Ace'
+    when 'K' then 'King'
+    when 'Q' then 'Queen'
+    when 'J' then 'Jack'
+    else
+      @value
     end
-    @cards.shuffle
+  end
+
+  def suit
+    case @suit
+    when 'H' then 'Hearts'
+    when 'C' then 'Clubs'
+    when 'D' then 'Diamonds'
+    when 'S' then 'Spades'
+    end
+  end
+
+  def ace?
+    value == 'Ace'
+  end
+
+  def face?
+    value == 'King' || value == 'Queen' || value == 'Jack'
+  end
+
+  def to_s
+    "The #{value} of #{suit}."
   end
 end
 
-class BlackJackGame
-  attr_reader :deck
+class Dealer
+  attr_accessor :cards
 
   def initialize
-    @deck = Deck.new
+    @name = 'Dealer'
+    @cards = []
   end
 end
 
 class Player
-  def iniitialize
+  attr_reader :name
+  attr_accessor :cards
+
+  def initialize(name)
+    @name = name
+    @cards = []
   end
 
   def hit
@@ -70,5 +93,101 @@ class Player
 
 end
 
+class Deck
+  attr_reader :cards
+  
+  def initialize
+    @cards = []
+    suits = %w[H D S C]
+    card_values = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
+    suits.each do |suit|
+      card_values.each do |value|
+        cards << Card.new(suit, value)
+      end
+    end
+    @cards.shuffle!
+  end
+end
+
+class BlackJackGame
+  attr_reader :deck, :player, :dealer
+
+  def initialize
+    @deck = Deck.new
+    @player = Player.new('Player')
+    @dealer = Dealer.new
+  end
+
+  def deal_cards
+    2.times do 
+      player.cards << @deck.cards.pop
+      dealer.cards << @deck.cards.pop
+    end
+  end
+
+  def display_message(message)
+    puts message
+  end
+
+  def player_total
+    total = 0
+    player.cards.each do |card|
+      if card.ace?
+        total += 11
+      elsif card.face?
+        total += 10
+      else
+        total += card.value.to_i
+      end
+    end
+    total
+  end
+
+  def player_turn
+    display_message("#{player.name} score is #{player_total} ")
+    
+    loop do
+      display_message("What do you want to do? [h = hit, s = stay]")
+      answer = nil
+      loop do        
+        answer = gets.chomp
+        break if %w[h s].include? answer
+        display_message('That is not a valid answer - try again.')
+      end
+
+      if answer == 'h'
+        player.cards << deck.cards.pop
+        puts player.cards
+      end
+      break
+    end
+  end
+
+  def show_initial_cards
+    show_cards(player.cards, dealer.cards)
+  end
+
+  def show_cards(player_cards, dealer_cards)
+    puts "Player has:"
+    player_cards.each do |card|
+      display_message(card)
+    end
+    puts ""
+    puts "Dealer has:"
+    dealer_cards.each do |card|
+      display_message(card)
+    end
+    puts ""
+  end
+
+  def start
+    deal_cards
+    show_initial_cards
+    player_turn
+#   dealer_turn
+#   show_result
+  end
+end
+
 twenty_one = BlackJackGame.new
-p twenty_one.deck.cards
+p twenty_one.start
